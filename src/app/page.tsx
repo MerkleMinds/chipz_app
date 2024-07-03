f3d63a683d0195b17fb169728149ef0a4dec1eb7
Javier R <javier.rios-urbano@epitech.eu>
2024-07-04 01:01:01 +0200

    add: betslip react context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,36 @@
-import Hot from "@/components/Hot";
-import Trending from "@/components/Trending";
+"use client";
 import HeadToHead from "@/components/HeadToHead";
 import Footer from "@/components/Footer";
 import Partners from "@/components/Partners";
+import League from "@/components/League";
+import Live from "@/components/Live";
+import Featured from "@/components/Featured";
+import { context as BetSlipContext } from "@/components/Utils";
+import { useState } from "react";
+import Betslip, { IBetSlipBet } from "@/components/BetSlip";
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+  const [bets, setBets] = useState<IBetSlipBet[]>([]);
+
   return (
-    <main className="flex flex-col gap-5">
-      <Hot />
-      <Trending />
-      <HeadToHead />
-      <Partners />
-      <Footer />
-    </main>
+    <BetSlipContext.Provider
+      value={{
+        bets,
+        setBets,
+        show,
+        setShow,
+      }}
+    >
+      <main className="flex flex-col gap-5">
+        <League />
+        <Featured />
+        <Live />
+        <HeadToHead />
+        <Partners />
+        <Footer />
+      </main>
+      <Betslip />
+    </BetSlipContext.Provider>
   );
 }
