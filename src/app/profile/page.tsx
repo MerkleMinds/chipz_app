daf34700f5cad2dff747339f34cc25791b2e1ea5
Javier R <javier.rios-urbano@epitech.eu>
2024-07-15 01:37:47 +0200

    add: `banner` with points

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,46 +1,54 @@
 "use client";
 
+import Banner from "@/components/profile/Banner";
 import Deposit from "@/components/profile/Deposit";
 import History from "@/components/profile/History";
 import Settings from "@/components/profile/Settings";
 import Social from "@/components/profile/Social";
 import Withdraw from "@/components/profile/Withdraw";
+import { useAppContext } from "@/components/Context";
 import { useState } from "react";
 
 const sections = {
   deposit: Deposit,
   withdraw: Withdraw,
   history: History,
-  Social: Social,
+  social: Social,
   settings: Settings,
 };
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState<keyof typeof sections>("deposit");
   const ActiveSection = sections[activeTab];
+  const { points: [points] } = useAppContext();
 
   return (
-    <div className="bg-gray-900 text-white mx-6 mt-2 mb-16">
-      <div className="max-w-md mx-auto">
-        <div className="flex justify-between mb-6 gap-2">
-          {(Object.keys(sections) as Array<keyof typeof sections>).map(
-            (tab) => (
-              <button
-                key={tab}
-                className={`flex-1 py-2 transition-colors duration-300 ease-in-out rounded-md text-xs ${
-                  activeTab === tab
-                    ? "text-white bg-gray-800"
-                    : "text-neutral-400 hover:text-white"
-                }`}
-                onClick={() => setActiveTab(tab)}
-              >
-                {tab.charAt(0).toUpperCase() + tab.slice(1)}
-              </button>
-            ),
-          )}
-        </div>
-        <ActiveSection />
+    <>
+      <div className="mx-6 mt-2 mb-4">
+        <Banner name="Guest User" points={points} />
       </div>
-    </div>
+      <div className="bg-gray-900 text-white mx-6 mt-2 mb-16">
+        <div className="max-w-md mx-auto">
+          <div className="flex justify-between mb-4 gap-2">
+            {(Object.keys(sections) as Array<keyof typeof sections>).map(
+              (tab) => (
+                <button
+                  key={tab}
+                  className={`flex-1 py-2 transition-colors duration-300 ease-in-out rounded-md text-xs ${
+                    activeTab === tab
+                      ? "text-white bg-gray-800"
+                      : "text-neutral-400 hover:text-white"
+                  }`}
+                  onClick={() => setActiveTab(tab)}
+                >
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                </button>
+              ),
+            )}
+          </div>
+          <ActiveSection />
+        </div>
+      </div>
+    </>
   );
 }
---
+++ src/components/profile/Banner.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import { useState } from "react";
+
+interface IBannerProps {
+  name: string;
+  points: number;
+}
+
+export default function Banner({ name, points }: IBannerProps) {
+  const [show, setShow] = useState<boolean>(true);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="relative w-full rounded-md bg-gray-800 p-2 flex flex-col gap-1">
+      <button
+        className="absolute top-2 right-2 text-white"
+        onClick={() => setShow(false)}
+      >
+        <FaTimes />
+      </button>
+      <p className="text-neutral-400">
+        Hi <span className="text-white font-bold">{name}</span>,
+      </p>
+      <p className="text-neutral-400 text-sm">
+        you have <span className="font-bold">{points}</span>{" "}
+        points to spend at the{" "}
+        <Link href={"/promo#shop"} className="text-neutral-400 underline">
+          shop
+        </Link>
+      </p>
+    </div>
+  );
+}
