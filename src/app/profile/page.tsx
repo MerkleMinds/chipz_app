420f9edaf98a6bb6e87fcd970e0519dde7d82044
Javier R <javier.rios-urbano@epitech.eu>
2024-07-22 21:23:29 +0200

    fix: use new `deposit` and `withdraw` section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,32 +1,37 @@
 "use client";
 
 import Banner from "@/components/profile/Banner";
-import Deposit from "@/components/profile/Deposit";
 import History from "@/components/profile/History";
+import Minipay from "@/components/profile/Minipay";
 import Settings from "@/components/profile/Settings";
 import Social from "@/components/profile/Social";
-import Withdraw from "@/components/profile/Withdraw";
 import { useAppContext } from "@/components/Context";
+import useGetAddress from "@/hooks/useGetAddress";
 import { useState } from "react";
 
 const sections = {
-  deposit: Deposit,
-  withdraw: Withdraw,
+  minipay: Minipay,
   history: History,
   social: Social,
   settings: Settings,
 };
 
 export default function Page() {
-  const [activeTab, setActiveTab] = useState<keyof typeof sections>("deposit");
+  const [activeTab, setActiveTab] = useState<keyof typeof sections>("minipay");
   const ActiveSection = sections[activeTab];
   const { points: [points] } = useAppContext();
+  const address = useGetAddress();
 
   return (
     <>
-      <div className="mx-6 mt-2 mb-4">
-        <Banner name="Guest User" points={points} />
-      </div>
+      {address && (
+        <div className="mx-6 mt-2 mb-4">
+          <Banner
+            name={`${address.slice(0, 6)}...${address.slice(0, 4)}`}
+            points={points}
+          />
+        </div>
+      )}
       <div className="bg-gray-900 text-white mx-6 mt-2 mb-16">
         <div className="max-w-md mx-auto">
           <div className="flex justify-between mb-4 gap-2">
