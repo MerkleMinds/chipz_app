a642b3dad809bc369c03b2aad28fb8b641c61fbb
Javier R <javier.rios-urbano@epitech.eu>
2024-07-22 21:28:30 +0200

    fix: remove `unused` import

--- src/components/profile/Social.tsx
+++ src/components/profile/Social.tsx
@@ -1,4 +1,4 @@
-import { FaClipboard, FaClipboardList, FaShare, FaUser } from "react-icons/fa6";
+import { FaClipboard, FaShare, FaUser } from "react-icons/fa6";
 
 import { hashBet } from "@/components/bets/Betv2";
 
