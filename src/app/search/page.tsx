0afad093288860a2451d5d8ef5d9fe8382be410c
Mateo Pizzolo <matpizzolo@gmail.com>
2025-04-30 11:30:13 -0300

    Search page, nueva funcionalidad, mas bets

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,10 @@
-import Box, { type Item } from "@/components/search/Box";
-import available from "@/utils/data/available.json" with { type: "json" };
+import Box, { type SearchItem } from "@/components/search/Box";
+import searchData from "@/utils/data/searchData.json" with { type: "json" };
 
 export default function Page() {
   return (
     <div className="w-full flex items-center justify-center">
-      <Box items={available as Item[]} />
+      <Box items={searchData as SearchItem[]} />
     </div>
   );
 }
